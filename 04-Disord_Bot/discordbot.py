8af5a1f6bb7448d49a91a71d5282f90b89feca8b
34773 <111361813+34773@users.noreply.github.com>
2022-11-10 12:43:25 +0100

    Discord bot

--- 04-Disord_Bot/discordbot.py
+++ 04-Disord_Bot/discordbot.py
@@ -3,7 +3,7 @@ import discord
 intents = discord.Intents.default()
 intents.messages= True
 client = discord.Client(intents=intents)
-bottoken=open("c:/geheim/discordbot.txt", "r").readline()
+bottoken=open("c:/geheim/replace.txt", "r").readline()
 
 @client.event
 async def on_ready():
@@ -14,6 +14,20 @@ async def on_ready():
 
     # We printen de naam van de bot user uit.
     print(client.user, "has connected to the client")
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    await channel.send("I'm online!")
+    await channel.send("How are yall?")
+
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author,"is the user who wrote the message")
+    print(message.created_at,"is when the message was posted")
+    print(message.channel,"is the channel this message was posted in")
+    if message.author.bot == False:
+        await message.channel.send("Hello " + str(message.author))
 
 
 client.run(bottoken)
